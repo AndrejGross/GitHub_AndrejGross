1d3743982e5490126512ea33847ee7b37c91611e
Andrej Gross <andrej99991111@gmail.com>
2018-12-03 21:35:09 +0100

    Funkcia a (hľadanie vyhovujúcich + aktualizácia)

--- main.c
+++ main.c
@@ -211,6 +211,33 @@ struct inzeraty* a(FILE **file,int *pocet_zaznamov,struct inzeraty *prvy,struct 
         {if (znacka_auta[j]<'A'||znacka_auta[j]>'Z')
                 znacka_auta[j]=znacka_auta[j]-32;
                 j++;}
+    act=prvy;
+    while(znacka_auta[j])
+        {if (znacka_auta[j]<'A'||znacka_auta[j]>'Z')
+                znacka_auta[j]=znacka_auta[j]-32;
+            j++;}
+
+    while(act!=NULL)
+    {
+        i=0;
+        sprintf(act_znacka,"%s",act->znacka);
+        while(act_znacka[i])
+        {
+            if (act_znacka[i]<'A'||act_znacka[i]>'Z')
+                act_znacka[i]=act_znacka[i]-32;
+            i++;}
+        if(strcmp(act_znacka,znacka_auta)==0&&act->rok_vyroby==rokVyroby)
+        {
+            if(act->cena-100<0)
+                act->cena=0;
+                else
+                act->cena-=100;
+            pocitadlo++;
+        }
+    act=act->dalsi;
+    }
+    printf("Aktualizovalo sa %d zaznamov\n",pocitadlo);
+return prvy;
 }
 
 int main()
