24cb20a3f6e545c7580b6efcad01b1712c00439e
Andrej Gross <andrej99991111@gmail.com>
2018-12-03 14:20:44 +0100

    First commit

---
+++ main.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+
+struct inzeraty
+{
+    char kategoria[51];
+    char znacka[51];
+    char predajca[101];
+    int cena;
+    int rok_vyroby;
+    char stav_vozidla[201];
+    struct inzeraty *dalsi;
+};
+
